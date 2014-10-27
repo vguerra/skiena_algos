d6b377152956372e1a33db820976da730e2b8197
Victor Guerra <vguerra@gmail.com>
2014-10-27 12:36:54 +0100

    test routines for dynamic array class.

---
+++ data_structures/dynamic_array_test.cc
@@ -0,0 +1,11 @@
+// Victor Guerra <vguerra@gmail.com>
+#define CATCH_CONFIG_MAIN
+#include <catch.hpp>
+#include "dynamic_array.h"
+
+
+TEST_CASE("Constructors of dynamic_array", "[dynamic_array]") {
+  SECTION("Default constructors") {
+    dynamic_array da;
+  }
+}
