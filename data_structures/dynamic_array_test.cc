7b360cf633c6530b5a2197860c29ec1a28459172
Victor Guerra <vguerra@gmail.com>
2014-10-28 22:54:58 +0100

    Baby steps on testing dynamic_array class: loging size and capacity.

--- data_structures/dynamic_array_test.cc
+++ data_structures/dynamic_array_test.cc
@@ -4,5 +4,13 @@
 #include "dynamic_array.h"
 
 TEST_CASE("Constructors of dynamic_array", "[dynamic_array]") {
-  SECTION("Default constructors") { dynamic_array da; }
+  SECTION("Default constructors") { 
+    {
+      dynamic_array da(2);
+  
+      WARN("The capacity is: " << da.capacity());
+      WARN("The size is: " << da.size());
+  
+    }
+  }
 }
